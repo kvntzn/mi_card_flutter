24af4ce5dde1e3a7d6cc9eba665b253043cd2dfd
heathkev <kevintuazon45@gmail.com>
2020-08-20 12:29:07 +0800

    added fonts and entries

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,71 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-             CircleAvatar(
-               radius: 50,
-               backgroundImage: AssetImage('images/kev.jpg'),
-             ),
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/kev.jpg'),
+              ),
               Text(
                 'Kevin Tuazon',
                 style: TextStyle(
-                  fontSize: 40,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
+                    fontFamily: 'Pacifico',
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'MOBILE DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color:Colors.teal
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('+639382738615',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20
+                      ),
+                    )
+                  ],
+                )
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  children: [
+                    Icon(
+                        Icons.mail,
+                        color: Colors.teal
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('kevintuazon45@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20
+                    ),
+                    )
+                  ]
                 ),
               )
             ],
@@ -32,5 +87,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
